fc417fe054afc26ba25371b56b3193438c783947
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-26 17:35:31 +0300

    Add range and rainbow brush function

--- script.js
+++ script.js
@@ -1,22 +1,112 @@
 const container = document.querySelector('.container');
 
+const create = document.querySelector('.create');
+
 function createGrid(num) {
+	let width = 600 / num;
+	num *= num;
+	let border = width / 50;
 	while (num > 0) {
 		let square = document.createElement('div');
+		square.style.width = `${width}px`;
+		square.style.height = `${width}px`;
+		square.style.borderWidth = `${border}px`;
 		square.classList.add('square');
 		container.appendChild(square);
 		num--;
 	}
 }
 
-createGrid(64);
+createGrid(10);
 
-const grid = document.querySelectorAll('.square');
-
-grid.forEach((div) => {
-	div.addEventListener('mouseover', () => {
-		if (div.classList.value.indexOf('color' === -1)) {
-			div.classList.add('color');
-		}
+function removeGrid() {
+	grid.forEach((div) => {
+		container.removeChild(div);
 	});
+}
+
+let grid = document.querySelectorAll('.square');
+
+const sizeRange = document.querySelector('#range');
+sizeRange.addEventListener('change', () => {
+	let size = sizeRange.value;
+	removeGrid();
+	createGrid(size);
+	document.querySelector('label').innerText = `Size ${size} x ${size}`;
+	grid = document.querySelectorAll('.square');
+	rainbow();
 });
+
+function black() {
+	grid.forEach((div) => {
+		div.addEventListener('mouseover', () => {
+			if (div.classList.value.indexOf('color' === -1)) {
+				div.classList.add('color');
+			}
+		});
+	});
+}
+
+function rainbow() {
+	const colors = [
+		'#FF6633',
+		'#FFB399',
+		'#FF33FF',
+		'#FFFF99',
+		'#00B3E6',
+		'#E6B333',
+		'#3366E6',
+		'#999966',
+		'#99FF99',
+		'#B34D4D',
+		'#80B300',
+		'#809900',
+		'#E6B3B3',
+		'#6680B3',
+		'#66991A',
+		'#FF99E6',
+		'#CCFF1A',
+		'#FF1A66',
+		'#E6331A',
+		'#33FFCC',
+		'#66994D',
+		'#B366CC',
+		'#4D8000',
+		'#B33300',
+		'#CC80CC',
+		'#66664D',
+		'#991AFF',
+		'#E666FF',
+		'#4DB3FF',
+		'#1AB399',
+		'#E666B3',
+		'#33991A',
+		'#CC9999',
+		'#B3B31A',
+		'#00E680',
+		'#4D8066',
+		'#809980',
+		'#E6FF80',
+		'#1AFF33',
+		'#999933',
+		'#FF3380',
+		'#CCCC00',
+		'#66E64D',
+		'#4D80CC',
+		'#9900B3',
+		'#E64D66',
+		'#4DB380',
+		'#FF4D4D',
+		'#99E6E6',
+		'#6666FF',
+	];
+
+	grid.forEach((div) => {
+		div.addEventListener('mouseover', () => {
+			let color = colors[Math.floor(Math.random() * colors.length)];
+			div.style.backgroundColor = color;
+		});
+	});
+}
+
+rainbow();
